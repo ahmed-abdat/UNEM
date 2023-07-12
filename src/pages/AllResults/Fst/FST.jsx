6e39992f9a5f567c005ab4a7e85cd373b5b07a2a
= <ahmedeabdat@gmail.com>
2023-07-12 18:14:42 +0000

    update new version

--- src/pages/AllResults/Fst/FST.jsx
+++ src/pages/AllResults/Fst/FST.jsx
@@ -1,13 +1,21 @@
 import Header from "../../../components/Header";
 import Footer from "../../../components/Footer";
-import './Fst.css'
+import "./Fst.css";
 
 export default function FST() {
+  const specialiter = [{ name: "BG" }, { name: "PC" }, { name: "MPI" } , { name: "BG" }, { name: "PC" }, { name: "MPI" } , { name: "BG" }, { name: "PC" }, { name: "MPI" }];
   return (
     <>
-      <Header picture={'/03.png'} />
+      <Header picture={"/03.png"} />
+      <section className="speciality">
+        {specialiter.map((item) => (
+          <div className="speciality-item" key={item.name}>
+            <h3>{item.name}</h3>
+          </div>
+        ))}
+      </section>
       <section className="fst">
-      <iframe src="https://drive.google.com/embeddedfolderview?id=17IdOq6c-90NuSqUJh3enxARpqAaSAbfh&resourcekey=RESOURCE-KEY" ></iframe>
+        <iframe src="https://drive.google.com/embeddedfolderview?id=17IdOq6c-90NuSqUJh3enxARpqAaSAbfh&resourcekey=RESOURCE-KEY"></iframe>
       </section>
       <Footer />
     </>
