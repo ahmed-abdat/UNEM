7fee614bb784531291e9cb0ff8d3b83d83ebc988
= <ahmedeabdat@gmail.com>
2023-07-12 14:36:08 +0000

    add link to whatsapp

--- src/pages/AllResults/Fst/FST.jsx
+++ src/pages/AllResults/Fst/FST.jsx
@@ -7,7 +7,7 @@ export default function FST() {
     <>
       <Header picture={'/03.png'} />
       <section className="fst">
-        <iframe src='https://chat.openai.com/' width="100%" height="100%" />
+        <iframe src='http://resultats.una.mr/' width="100%" height="100%" allowFullScreen={true} />
       </section>
       <Footer />
     </>
