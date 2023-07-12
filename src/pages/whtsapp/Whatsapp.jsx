05fc648107a634e9df0360af8265a70d17f4ee5d
= <ahmedeabdat@gmail.com>
2023-07-12 12:00:24 +0000

    add link to whatsapp

--- src/pages/whtsapp/Whatsapp.jsx
+++ src/pages/whtsapp/Whatsapp.jsx
@@ -2,28 +2,31 @@ import Header from "../../components/Header";
 import Footer from "../../components/Footer";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { db } from "../../config/firebase";
+import { collection, getDocs } from "firebase/firestore";
 import "./Whatsapp.css";
 import data from "../../data/bac.json";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { query } from "firebase/database";
 
 export default function Whatsapp() {
   const [isValid, setIsValid] = useState(false);
   const [numBac, setNumBac] = useState("");
   const [yearBorn, setYearBorn] = useState("");
-  const [seri, setSeri] = useState("");
-  const [validYear , setValidYear ] = useState(2000)
-
-  
+  const [student, setStudent] = useState("");
+  const [validYear, setValidYear] = useState(2000);
+  const [allExistingStd , setAllExistingStd] = useState(null)
 
   const handelValideStudent = (e) => {
     e.preventDefault();
     const isValid = data.find((student) => student.NumBac === +numBac);
     if (isValid) {
       setIsValid(true);
-      setSeri(isValid.Série);
+      setStudent(isValid);
+      console.log(isValid.DateNaissance);
       toast.success("تم التأكد");
-      setNumBac('')
-      return
+      setNumBac("");
+      return;
     }
 
     toast.info("للأسف يشترط في دخول المجموعة أن تكون ناجحا !");
@@ -38,29 +41,50 @@ export default function Whatsapp() {
   const handelYearBorn = (e) => {
     const { value } = e.target;
     let yearBornValue = value.slice(0, 4); // Restrict to 4 digits
-    // yearBornValue = yearBornValue.replace(/[^1-2]/g, ''); 
+    // yearBornValue = yearBornValue.replace(/[^1-2]/g, '');
     setYearBorn(yearBornValue);
   };
 
-
-
   const handelValideYearnBorn = (e) => {
-    e.preventDefault()
+    e.preventDefault();
+    const validYear = student.DateNaissance.slice(-4)
 
-    if(validYear === +yearBorn) {
-      toast.success('تم دخول الرابط')
-      console.log(seri);
-      // link here
-      return 
+
+   
+
+    if (validYear === yearBorn) {
+      console.log(student);
+         // handel whatsapp redirect
+
+      window.open(
+        "https://chat.whatsapp.com/FLUaAmyD1TwLl0VS4LPjzn"
+      );
+    
+      return;
     }
-    toast.error('يبدو بأن سنة الميلاد غير صحيحة')
-  }
+    toast.error("يبدو بأن سنة الميلاد غير صحيحة");
+  };
 
-  const handelDisabelBtn = () => {
-    if(yearBorn.length !== 4) return true
-    if(numBac.length !== 5 ) return true
+  // // get all docs from firebase
+  // const getAllDocs = async () => {
+  //   try {
+  //     // const q = query(collection(db, "students"));
+  //     const docsRef = collection(db, "students");
+  //     const querySnapshot = await getDocs(docsRef);
+  //     let allStds = []
+  //     querySnapshot.forEach((doc) => {
+  //       allStds.push({...doc.data() , id : doc.id})
+  //       // doc.data() is never undefined for query doc snapshots
+  //     });
+  //     setAllExistingStd(allStds)
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
 
-  } 
+  // useEffect(() => {
+  //   getAllDocs();
+  // }, []);
 
   return (
     <>
@@ -92,11 +116,25 @@ export default function Whatsapp() {
               </div>
             )}
           </div>
-          <div className="btn">
-            <button disabled={handelDisabelBtn()} onClick={isValid ? handelValideYearnBorn : handelValideStudent}>
-              {isValid ? "إنضمام" : "تأكد"}
-            </button>
-          </div>
+          {!isValid ? (
+            <div className="btn">
+              <button
+                disabled={numBac.length !== 5}
+                onClick={handelValideStudent}
+              >
+                تأكيد
+              </button>
+            </div>
+          ) : (
+            <div className="btn">
+              <button
+                disabled={yearBorn.length !== 4}
+                onClick={handelValideYearnBorn}
+              >
+                إنضمام
+              </button>
+            </div>
+          )}
         </form>
       </section>
       <ToastContainer
