72674c613adc6c549163edbd9135f07219f64a65
= <ahmedeabdat@gmail.com>
2023-07-12 16:48:12 +0000

    add link to whatsapp

--- src/pages/Branches/Branches.jsx
+++ src/pages/Branches/Branches.jsx
@@ -7,43 +7,43 @@ export default function Resulta() {
   const options = [
     {
       content: "كلية العلوم و التقنيات",
-      url: "https://wa.me/27245347?text=''",
+      url: "https://wa.me/27245347?",
     },
     {
       content: "كلية العلوم القانونية و الإقتصادية",
-      url: "https://wa.me/26031381?text=''",
+      url: "https://wa.me/26031381?",
     },
     {
         content : 'كلية العلوم القانونية و الإقتصادية (الملحق 2)' ,
-        url : "https://wa.me/49876770?text=''"
+        url : "https://wa.me/49876770?"
     },
     {
       content: "كلية الآداب و العلوم الإنسانية",
-      url: "https://wa.me/26490223?text=''",
+      url: "https://wa.me/26490223?",
     },
     {
       content: "المعهد العالي للمحاسبة و إدارة المؤسسات",
-      url: "https://wa.me/48572025?text=''",
+      url: "https://wa.me/48572025?",
     },
     {
       content: "المعهد العالي للدراسات و البحوث الإسلامية",
-      url: "https://wa.me/33300307?text=''",
+      url: "https://wa.me/33300307?",
     },
     {
       content: "المعهد الجامعي المهني",
-      url: "https://wa.me/49652477?text=''",
+      url: "https://wa.me/49652477?",
     },
     {
       content: "كلية الطب و الصيدلة",
-      url: "https://wa.me/47401121?text=''",
+      url: "https://wa.me/47401121?",
     },
     {
       content: "جامعة العلوم الإسلامية يالعيون",
-      url: "https://wa.me/36199323?text=''",
+      url: "https://wa.me/49637971?",
     },
     {
       content: "المعهد العالي للتعليم التكنلوجي بروصو",
-      url: "https://wa.me/36199323?text=''",
+      url: "https://wa.me/36199323?",
     },
     {
       content: "المعهد العالي المهني للغات و الترجمة و الترجمة الفورية",
