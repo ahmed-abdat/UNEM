72ddf231336daac2987bc07a8cd93a00c2003c00
= <ahmedeabdat@gmail.com>
2023-07-13 14:21:08 +0000

    fix new bugs

--- src/pages/calculation/index.jsx
+++ src/pages/calculation/index.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import Header from "../../components/Header";
 import CalculationSelect from "../../components/CalculationSelect";
-import CalculationLogo from "../../assets/4.svg";
 import Footer from "../../components/Footer";
 import Inputes from "../../components/Inputes";
 import "./calculation.css";
