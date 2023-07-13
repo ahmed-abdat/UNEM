1a6c6a954dda501b47140c9782fc635999c61450
= <ahmedeabdat@gmail.com>
2023-07-13 13:47:26 +0000

    fix new bugs

--- src/pages/AllInstitution/Fst/Fst.jsx
+++ src/pages/AllInstitution/Fst/Fst.jsx
@@ -3,7 +3,7 @@ import '../Fst.css'
 export default function Fst() {
   return (
     <section className="institutions-img">
-        <img src="/fac2/01.png" alt="fst" />
+        <img src="/fac2/01.jpg" alt="fst" />
     </section>
   )
 }
