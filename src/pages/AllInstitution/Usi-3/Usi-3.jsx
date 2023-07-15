89af5e3270c0e773b64f007d59932ba5fc383daf
= <ahmedeabdat@gmail.com>
2023-07-15 00:00:34 +0000

    add performance speed

--- src/pages/AllInstitution/Usi-3/Usi-3.jsx
+++ src/pages/AllInstitution/Usi-3/Usi-3.jsx
@@ -3,7 +3,7 @@ import '../Fst.css'
 export default function Fst() {
   return (
     <section className="institutions-img">
-        <img src="/fac2/09.jpg" alt="fst" />
+        <img src="/fac2/3.jpg" alt="fst" />
     </section>
   )
 }
