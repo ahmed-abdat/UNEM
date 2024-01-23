ae9314a27c81d40377c6213869795ed8ffef9de6
= <ahmedeabdat@gmail.com>
2024-01-23 18:50:53 +0000

    Update URL in SharePoste component

--- src/components/SharePoste.jsx
+++ src/components/SharePoste.jsx
@@ -1,7 +1,7 @@
 import { FacebookIcon, FacebookShareButton, WhatsappIcon, WhatsappShareButton } from "react-share";
 
 function SharePoste({ id}) {
-  const url = `https://unem.vercel.app/news/${id}`
+  const url = `https://unem.vercel.app/news/poste/${id}`
   return <section className="share">
     <FacebookShareButton url={url}>
         <FacebookIcon size={28} round={true} />
