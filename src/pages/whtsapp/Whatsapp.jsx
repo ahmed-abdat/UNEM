9cc2dc26d211f53230150674ea352e1bea77f4f3
= <ahmedeabdat@gmail.com>
2024-01-23 18:05:41 +0000

    Remove unused import and fix typo in Whatsapp.jsx

--- src/pages/whtsapp/Whatsapp.jsx
+++ src/pages/whtsapp/Whatsapp.jsx
@@ -4,7 +4,6 @@ import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "./Whatsapp.css";
 import data from "../../data/BAC2023VALID.json";
-import Sesion from '../../data/SessionBac.json'
 import { useRef, useState } from "react";
 
 export default function Whatsapp() {
@@ -13,7 +12,7 @@ export default function Whatsapp() {
 
   const handelValideStudent = (e) => {
     e.preventDefault();
-    const isValid = data.find((student) => student.NODOSS === numBac) || Sesion.find(student => student.NODOSS === numBac);
+    const isValid = data.find((student) => student.NODOSS === numBac) ;
     
     if (isValid) {
       const whtspUrl = {
