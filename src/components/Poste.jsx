005840bfa01cb074aaabf9b22ee1cb7cb0fc4ff7
= <ahmedeabdat@gmail.com>
2024-01-24 22:23:26 +0000

    Add file-saver dependency and implement image selection feature

--- src/components/Poste.jsx
+++ src/components/Poste.jsx
@@ -7,14 +7,17 @@ import Header from "./Header";
 import "./styles/poste.css";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { showTime} from '../utils/showTime'
+import { showTime } from "../utils/showTime";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import SharePoste from "./SharePoste";
 import PosteSkelton from "./PosteSkelton";
+import ViewFullImage from "./ViewImage";
 function poste() {
   const { id } = useParams();
   const [poste, setPoste] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
+  const [isImageSelected, setIsImageSelected] = useState(false);
+  const [image, setImage] = useState(null);
   const firestore = getFirestore(app);
   const getPoste = async (id) => {
     const docRef = doc(firestore, "postes", id);
@@ -40,78 +43,94 @@ function poste() {
   // check if the is mobile or not
   const isMobile = window.innerWidth <= 768;
 
-  const url = `https://unem.vercel.app/news/poste/${id}`
+  const url = `https://unem.vercel.app/news/poste/${id}`;
   const handelCopy = () => {
     navigator.clipboard.writeText(url);
-    toast.success('تم نسخ الرابط بنجاح')
-  }
+    toast.success("تم نسخ الرابط بنجاح");
+  };
 
-
+  // handel selected image
+  const selectedImage = (media) => {
+    if (media) {
+      setImage(media);
+      setIsImageSelected(true);
+    }
+  };
 
   return (
     <>
-      <Header />
-      <ToastContainer
-          position="top-center"
-          autoClose={2000}
-          newestOnTop={false}
-          closeOnClick
-          rtl
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-          hideProgressBar
-          theme="light"
-          transition:Zoom
+      {isImageSelected ? (
+        <ViewFullImage
+          selectedImage={image}
+          setIsImageSelected={setIsImageSelected}
+          images={poste.images}
         />
-      {/* <PosteSkelton /> */}
-      {
-        isLoading ? <PosteSkelton /> : <section className="poste">
-        <div className="poste-title">
-          <h1>{poste?.title}</h1>
-        </div>
-        <div className="poste-thumbnail">
-          <LazyLoadImage
-            src={poste?.images[0].url}
-            alt={poste?.title}
-            height={isMobile ? 300 : 420}
-            effect="blur"
-            placeholderSrc="/image_loaders.gif"
-            width={`100%`}
+      ) : (
+        <>
+          <Header />
+          <ToastContainer
+            position="top-center"
+            autoClose={2000}
+            newestOnTop={false}
+            closeOnClick
+            rtl
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            hideProgressBar
+            theme="light"
+            transition:Zoom
           />
-        </div>
-        <div className="poste-info">
-          <div className="poste-times">
-            <span className="create-time">
-            {showTime(poste?.createdAt)} 
-            </span>
-            <span className="bar">|</span>
-            <span className="update-time">
-            آخر تحديث: 24/1/2024
-            </span>
-          </div>
-          <div className="poste-share">
-            <SharePoste handelCopy={handelCopy} url={url}/>
-          </div>
-        </div>
-        <div className="poste-content">
-          <p>{poste?.description}</p>
-        </div>
-        <div className="poste-images">
-          {poste?.images.map((image) => (
-            <LazyLoadImage
-              key={image.url}
-              src={image.url}
-              alt={poste?.title}
-              effect="blur"
-              placeholderSrc="/image_loaders.gif"
-              width={`100%`}
-              // height={isMobile ? 300 : 420}
-            />
-          ))}
-        </div>
-      </section>
-      }
+          {isLoading ? (
+            <PosteSkelton />
+          ) : (
+            <section className="poste">
+              <div className="poste-title">
+                <h1>{poste?.title}</h1>
+              </div>
+              <div className="poste-thumbnail">
+                <LazyLoadImage
+                  src={poste?.images[0].url}
+                  alt={poste?.title}
+                  height={isMobile ? 300 : 420}
+                  effect="blur"
+                  placeholderSrc="/image_loaders.gif"
+                  width={`100%`}
+                />
+              </div>
+              <div className="poste-info">
+                <div className="poste-times">
+                  <span className="create-time">
+                    {showTime(poste?.createdAt)}
+                  </span>
+                  <span className="bar">|</span>
+                  <span className="update-time">آخر تحديث: 24/1/2024</span>
+                </div>
+                <div className="poste-share">
+                  <SharePoste handelCopy={handelCopy} url={url} />
+                </div>
+              </div>
+              <div className="poste-content">
+                <p>{poste?.description}</p>
+              </div>
+              <div className="poste-images">
+                {poste?.images.map((image) => (
+                  <LazyLoadImage
+                    key={image.url}
+                    src={image.url}
+                    alt={poste?.title}
+                    effect="blur"
+                    placeholderSrc="/image_loaders.gif"
+                    width={`100%`}
+                    onClick={() => selectedImage(image)}
+                    // height={isMobile ? 300 : 420}
+                  />
+                ))}
+              </div>
+            </section>
+          )}
+        </>
+      )}
     </>
   );
 }
---
+++ src/components/ViewImage.jsx
@@ -0,0 +1,128 @@
+import { MdClose } from "react-icons/md";
+import { useCallback, useEffect, useState } from "react";
+import { BiChevronLeft, BiChevronRight } from "react-icons/bi";
+import React from "react";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import {FiDownload} from "react-icons/fi"
+import { saveAs } from 'file-saver'
+import "./styles/ViewFullImage.css";
+import "./styles/Swiper.css";
+
+
+export default function ViewFullImage({ selectedImage, setIsImageSelected , images}) {
+
+    console.log(images , selectedImage , setIsImageSelected );
+
+
+  // handel Back
+  const handelBack = () => {
+    setIsImageSelected(false);
+  };
+
+  
+
+
+  const selectedImageIndex = images.findIndex(
+    (image) => image.src === selectedImage.src
+  );
+
+  // track the inedx of the selected image
+  const [imageIndex, setImageIndex] = useState(selectedImageIndex);
+  const [isLastIndex, setIsLastIndex] = useState(false);
+  const [isFirstIndex, setIsFirstIndex] = useState(false);
+  const [isArrowShow, setIsArrowShow] = useState(true);
+  
+
+  // handel next image
+  const handelPrevImage = () => {
+    if (imageIndex === images.length - 1) return;
+    setImageIndex((prevImageIndex) => prevImageIndex + 1);
+  };
+
+  // handel prev image
+  const handelNextImage = () => {
+    if (imageIndex === 0) return;
+    setImageIndex((prevImageIndex) => prevImageIndex - 1);
+  };
+
+  //   check if the current image index is the first index or the last index
+  useEffect(() => {
+    if (imageIndex === 0) {
+      setIsLastIndex(true);
+    } else {
+      setIsLastIndex(false);
+    }
+    if (imageIndex === images.length - 1) {
+      setIsFirstIndex(true);
+    } else {
+      setIsFirstIndex(false);
+    }
+  }, [imageIndex, images.length]);
+
+
+
+
+
+
+  // handel download image
+  const downloadImage = () => {
+    const imageURL = images[imageIndex].url;
+    const imageName = imageURL?.split('?')[0].split('/')[7];
+    saveAs(imageURL, imageName);
+  } 
+
+  // sort the images by the 
+
+
+
+  return (
+    <div className="viewFullPage">
+      <div className="header">
+        <div className="icon" onClick={handelBack}>
+          <MdClose />
+        </div>
+            <div className="icon">
+            <FiDownload  onClick={downloadImage}/>
+            </div>
+      </div>
+      <div className="image d-f">
+      <div className="swiper--container">
+      {/* render the current image and add a cursor to navigate between images */}
+      <div
+        className={`swiper--wrapper`}
+        style={{ transform: `translateX(-${imageIndex * 100}%)` }}
+      >
+        {images.map((image) => (
+          <div key={image.url} className="swiper--image">
+            {/* <img src={image.src} alt="" onClick={()=> setIsArrowShow(prev => !prev)} /> */}
+            <LazyLoadImage
+              alt="image"
+              height={"100%"}
+              src={image.url}
+              onClick={()=> setIsArrowShow(prev => !prev)}
+              width={"100%"}
+              effect="blur"
+            />
+
+            {(  isArrowShow && !isLastIndex) && (
+              <div className={`arrow--container next`} onClick={handelNextImage}>
+                <div className="swiper--next d-f" >
+                <BiChevronLeft />
+              </div>
+              </div>
+            )}
+            {(  isArrowShow && !isFirstIndex) && (
+             <div className={`arrow--container prev`} onClick={handelPrevImage}>
+               <div className="swiper--prev d-f" >
+                <BiChevronRight />
+              </div>
+             </div>
+            )}
+          </div>
+        ))}
+      </div>
+    </div>
+      </div>
+    </div>
+  );
+}
