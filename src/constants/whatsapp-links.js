9a548b13f30fc6c047214b091790e375654fd22e
Ahmed Abdellahi Abdat <ahmedeabdat@gmail.com>
2025-08-05 11:02:11 +0100

    Update whatsapp-links.js

--- src/constants/whatsapp-links.js
+++ src/constants/whatsapp-links.js
@@ -3,7 +3,7 @@ export const whatsAppGroups = {
   M: "https://chat.whatsapp.com/IlOCSN4SDWlB2qYE6xl62C",
   TMGM: "https://chat.whatsapp.com/IlOCSN4SDWlB2qYE6xl62C",
   TSGE: "https://chat.whatsapp.com/IlOCSN4SDWlB2qYE6xl62C",
-  LO: "https://chat.whatsapp.com/Gdfaer8t9sAEmPTlcInL1V",
+  LO: "https://chat.whatsapp.com/DDhcq7GlhF4LCRJNwbWrei",
   LA: "https://chat.whatsapp.com/KqH9Rt9OlFE9BrCRatiZxf",
   LM: "https://chat.whatsapp.com/KqH9Rt9OlFE9BrCRatiZxf",
   TM: "https://chat.whatsapp.com/DIhrzUz41anDCQpSbcsIDh",
